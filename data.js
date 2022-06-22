c7c17b61921eeeee68d0787fb0126085bf38748c
TONYCHIDI <naccyworld@gmail.com>
2022-06-22 17:12:51 +0100

    printing the output on the screen

--- data.js
+++ data.js
@@ -6,13 +6,12 @@ let myMiddleNmae = "CHIDI";
 let myHeight = "1.76 meters";
 let myCountry = "NIGERIA";
 
-let myFullName = `${myLastName} ${myFirstName} ${myMiddleNmae}`;
-alert("My name is " + myFullName);
-
 let myHeightDetails = "I am " + myHeight + " tall";
-alert(myHeightDetails);
-
 let myCountryDescription = "I am from Federal Republic of " + myCountry;
-alert(myCountryDescription);
+let myFullName = `${myLastName} ${myFirstName} ${myMiddleNmae}`;
+
+alert("My name is " + myFullName + "\n" +
+    myHeightDetails + "\n" +
+    myCountryDescription);
 
 // The url of my Github pages is https://tonychidi.github.io/About-me/
