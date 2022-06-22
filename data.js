fd59e53b417e5e1781ebd26d4b495fcb5478614d
TONYCHIDI <naccyworld@gmail.com>
2022-06-22 16:59:22 +0100

    removal of html css

--- data.js
+++ data.js
@@ -7,12 +7,12 @@ let myHeight = "1.76 meters";
 let myCountry = "NIGERIA";
 
 let myFullName = `${myLastName} ${myFirstName} ${myMiddleNmae}`;
-document.getElementById("my-name").innerHTML = "My name is " + myFullName;
+alert("My name is " + myFullName);
 
 let myHeightDetails = "I am " + myHeight + " tall";
-document.getElementById("my-height").innerHTML = myHeightDetails;
+alert(myHeightDetails);
 
 let myCountryDescription = "I am from Federal Republic of " + myCountry;
-document.getElementById("my-country").innerHTML = myCountryDescription;
+alert(myCountryDescription);
 
 // The url of my Github pages is https://tonychidi.github.io/About-me/
